ed0a8279648b4a2504f2ef985ec5d9f708d8f926
proFFF <v.dunin@gmail.com>
2020-10-18 12:39:26 +0300

    Clean old experimental code

--- src/main/java/ru/profff/sandbox/MyFirstProgram.java
+++ src/main/java/ru/profff/sandbox/MyFirstProgram.java
@@ -2,25 +2,8 @@ package ru.profff.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-
         System.out.println("Hello world");
-        //math_experiments();
-        int l =8;
-        System.out.println("Площадь квадрата со стороной " + l + "равна:" + (l*l) );
+
     }
 
-    private static void math_experiments() {
-        System.out.println(2 + 2);
-        System.out.println(2 - 2);
-        System.out.println(2*2);
-        System.out.println(1/2);
-        System.out.println(1.0/2);
-        System.out.println(1/2.0);
-        System.out.println("2" + "2");
-        System.out.println("2" + 2);
-        System.out.println(2 + "2");
-        System.out.println((2 + 2)*2);
-        System.out.println("2 + 2 =" + 2 +2);
-        System.out.println("2 + 2 = " + (2+2));
-    }
 }
