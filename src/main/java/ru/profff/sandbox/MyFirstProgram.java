26d4a261d3c7e1588abd941a603c9dbb082bb92b
proFFF <v.dunin@gmail.com>
2020-10-18 12:16:18 +0300

    Переменные

--- src/main/java/ru/profff/sandbox/MyFirstProgram.java
+++ src/main/java/ru/profff/sandbox/MyFirstProgram.java
@@ -4,6 +4,12 @@ public class MyFirstProgram {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+        //math_experiments();
+        int l =8;
+        System.out.println("Площадь квадрата со стороной " + l + "равна:" + (l*l) );
+    }
+
+    private static void math_experiments() {
         System.out.println(2 + 2);
         System.out.println(2 - 2);
         System.out.println(2*2);
