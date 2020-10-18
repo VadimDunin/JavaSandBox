9679ab8a59b60acf0f5397f01b2010cffee3f103
proFFF <v.dunin@gmail.com>
2020-10-18 12:52:23 +0300

    Функциии и пример с одинаковыми названиями функции, но разными параметрами

--- src/main/java/ru/profff/sandbox/MyFirstProgram.java
+++ src/main/java/ru/profff/sandbox/MyFirstProgram.java
@@ -2,8 +2,25 @@ package ru.profff.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        hello("Vadim");
+        double l = 10;
+        System.out.println("Площадь квадрата равна " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника равна "+ area(a, b));
 
     }
 
+    public static void hello(String somebody) {
+        //String somebody = "world";
+        System.out.println("Hello, " + somebody);
+    }
+
+    public static double area(double len) {
+        return len * len;
+    }
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }
