14cfd221b0d1bf6bd01eeaaebbd3eebbfbcbe544
proFFF <v.dunin@gmail.com>
2020-10-18 12:02:35 +0300

    math experiments

--- src/main/java/ru/profff/sandbox/MyFirstProgram.java
+++ src/main/java/ru/profff/sandbox/MyFirstProgram.java
@@ -1,8 +1,20 @@
 package ru.profff.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+
         System.out.println("Hello world");
+        System.out.println(2 + 2);
+        System.out.println(2 - 2);
+        System.out.println(2*2);
+        System.out.println(1/2);
+        System.out.println(1.0/2);
+        System.out.println(1/2.0);
+        System.out.println("2" + "2");
+        System.out.println("2" + 2);
+        System.out.println(2 + "2");
+        System.out.println((2 + 2)*2);
+        System.out.println("2 + 2 =" + 2 +2);
+        System.out.println("2 + 2 = " + (2+2));
     }
 }
